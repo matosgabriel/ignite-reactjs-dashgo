e9350198b23d6df63b93042102d29de956714320
matosgabriel <gabriel.ifsal@gmail.com>
2022-05-02 09:13:55 -0300

    Creating Home page and Input element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,45 @@
 import Head from 'next/head'
+import { Button, Flex, FormLabel, Stack, FormControl } from '@chakra-ui/react';
+import { Input } from '../components/Form/Input';
 
 export default function Home() {
   return (
-    <h1>Dashgo</h1>
-  )
+    <>
+    <Head>
+      <title>Home</title>
+    </Head>
+    
+    <Flex
+      w="100vw"
+      height="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        padding="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+
+        <Stack spacing="4">
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>  
+    </Flex>
+    </>
+  );
 }
