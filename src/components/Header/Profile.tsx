715fb43b125c7c771b117e807e1e11455dc8398e
matosgabriel <gabriel.ifsal@gmail.com>
2022-05-08 11:14:52 -0300

    Turning the Header component responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,22 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex
       align="center"
     >
-      <Box mr="4" textAlign="right">
-        <Text>Gabriel Matos</Text>
-        <Text color="gray.300" fontSize="small">
-          gabriel.ifsal@gmail.com
-        </Text>
-      </Box>
-
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gabriel Matos</Text>
+          <Text color="gray.300" fontSize="small">
+            gabriel.ifsal@gmail.com
+          </Text>
+        </Box>
+      ) }
       <Avatar size="md" name="Gabriel Matos" src="https://github.com/matosgabriel.png"></Avatar>
     </Flex>
   );
