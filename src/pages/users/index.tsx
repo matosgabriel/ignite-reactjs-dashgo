33a5b96019e34ba10595ce5a03630b0fbed1e932
Gabriel <gabriel.ifsal@gmail.com>
2022-08-24 17:21:56 -0300

    Configuring React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
+import { useQuery } from 'react-query';
 
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '~/components/Header';
@@ -9,16 +10,17 @@ import { Pagination } from '~/components/Pagination';
 import { Sidebar } from '~/components/Sidebar';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+
+    return data;
+  });
+  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   });
-  
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(response => console.log(response));
-  },  []);
 
   return (
     <>
@@ -48,45 +50,58 @@ export default function UserList() {
               </Link>
             </Flex>
 
-            <Table colorScheme="whiteAlpha">
-              <Thead>
-                <Tr>
-                  <Th px={["2", "3", "6"]} color="gray.300" width="8">
-                    <Checkbox colorScheme="pink" />
-                  </Th>
-                  <Th px={["2", "3", "6"]}>Usuários</Th>
-                  { isWideVersion && <Th>Data de cadastro</Th> }
-                  <Th width="8"></Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                <Tr>
-                  <Td px={["2", "3", "6"]}>
-                    <Checkbox colorScheme="pink" />
-                  </Td>
-                  <Td px={["2", "3", "6"]}>
-                    <Box>
-                      <Text fontWeight="bold">Gabriel Matos</Text>
-                      <Text fontSize="sm" color="gray.300">gabriel.ifsal@gmail.com</Text>
-                    </Box>
-                  </Td>
-                  { isWideVersion && <Td>19 de Março, 2001</Td>}
-                  <Td>
-                    <Button
-                      as="a"
-                      colorScheme="purple"
-                      size="sm"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                      pr={!isWideVersion && 1}
-                    >
-                      { isWideVersion && 'Editar' }
-                    </Button>
-                  </Td>
-                </Tr>
-              </Tbody>
-            </Table>
+            { isLoading ? (
+                <Flex justify='center'>
+                  <Spinner />
+                </Flex> 
+              ) : error ? (
+                <Flex justify='center'>
+                  <Text>Falha ao obter dados dos usuários</Text>
+                </Flex>
+              ) : (
+                <>
+                  <Table colorScheme="whiteAlpha">
+                  <Thead>
+                    <Tr>
+                      <Th px={["2", "3", "6"]} color="gray.300" width="8">
+                        <Checkbox colorScheme="pink" />
+                      </Th>
+                      <Th px={["2", "3", "6"]}>Usuários</Th>
+                      { isWideVersion && <Th>Data de cadastro</Th> }
+                      <Th width="8"></Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr>
+                      <Td px={["2", "3", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td px={["2", "3", "6"]}>
+                        <Box>
+                          <Text fontWeight="bold">Gabriel Matos</Text>
+                          <Text fontSize="sm" color="gray.300">gabriel.ifsal@gmail.com</Text>
+                        </Box>
+                      </Td>
+                      { isWideVersion && <Td>19 de Março, 2001</Td>}
+                      <Td>
+                        <Button
+                          as="a"
+                          colorScheme="purple"
+                          size="sm"
+                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                          pr={!isWideVersion && 1}
+                        >
+                          { isWideVersion && 'Editar' }
+                        </Button>
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
 
-            <Pagination />
+                <Pagination />
+                </>
+              )
+            }
           </Box>
         </Flex>
       </Box>
