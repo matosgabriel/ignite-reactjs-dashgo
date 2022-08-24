01348108f4562658654f1d22c930672a872172f7
Gabriel <gabriel.ifsal@gmail.com>
2022-08-24 18:24:04 -0300

    Setting stale time for users data and showing a spinner on refetch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,10 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '~/styles/theme';
 import { SidebarDrawerProvider } from '~/contexts/SidebarDrawerContext';
-import { QueryClientProvider, QueryClient } from 'react-query';
 import { makeServer } from '~/services/mirage';
+
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 if (process.env.NODE_ENV === 'development') {
   makeServer();
@@ -20,6 +22,8 @@ function MyApp({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </SidebarDrawerProvider>
       </ChakraProvider>
+
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
