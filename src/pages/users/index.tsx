eafe575d79912d223b1ff1278ef257a93bd251fa
Gabriel <gabriel.ifsal@gmail.com>
2022-08-24 17:43:38 -0300

    Showing the fetched data off fake api on users listing

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -10,11 +10,20 @@ import { Pagination } from '~/components/Pagination';
 import { Sidebar } from '~/components/Sidebar';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
+  const { data: users, isLoading, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user => ({
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      }),
+    }));
+
+    return users;
   });
   
   const isWideVersion = useBreakpointValue({
@@ -72,29 +81,33 @@ export default function UserList() {
                     </Tr>
                   </Thead>
                   <Tbody>
-                    <Tr>
-                      <Td px={["2", "3", "6"]}>
-                        <Checkbox colorScheme="pink" />
-                      </Td>
-                      <Td px={["2", "3", "6"]}>
-                        <Box>
-                          <Text fontWeight="bold">Gabriel Matos</Text>
-                          <Text fontSize="sm" color="gray.300">gabriel.ifsal@gmail.com</Text>
-                        </Box>
-                      </Td>
-                      { isWideVersion && <Td>19 de Março, 2001</Td>}
-                      <Td>
-                        <Button
-                          as="a"
-                          colorScheme="purple"
-                          size="sm"
-                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                          pr={!isWideVersion && 1}
-                        >
-                          { isWideVersion && 'Editar' }
-                        </Button>
-                      </Td>
-                    </Tr>
+                    { users.map(user => {
+                      return (
+                        <Tr key={user.id}>
+                          <Td px={["2", "3", "6"]}>
+                            <Checkbox colorScheme="pink" />
+                          </Td>
+                          <Td px={["2", "3", "6"]}>
+                            <Box>
+                              <Text fontWeight="bold">{user.name}</Text>
+                              <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                            </Box>
+                          </Td>
+                          { isWideVersion && <Td>{user.createdAt}</Td>}
+                          <Td>
+                            <Button
+                              as="a"
+                              colorScheme="purple"
+                              size="sm"
+                              leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                              pr={!isWideVersion && 1}
+                            >
+                              { isWideVersion && 'Editar' }
+                            </Button>
+                          </Td>
+                        </Tr>
+                      );
+                    }) }
                   </Tbody>
                 </Table>
 
