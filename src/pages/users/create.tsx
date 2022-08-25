dd8c716dc60737e72ecb56b4f668e619525be604
Gabriel <gabriel.ifsal@gmail.com>
2022-08-25 00:35:17 -0300

    Configuring serializer to insert users with mutation and adding toast notification as an insertion visual feedback

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -1,12 +1,16 @@
-import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react';
+import { Box, Button, Divider, Flex, Heading, HStack, SimpleGrid, VStack, useToast } from '@chakra-ui/react';
 import Link from 'next/link';
 import { Input } from '~/components/Form/Input';
 import { Header } from '~/components/Header';
 import { Sidebar } from '~/components/Sidebar';
 
+import { useMutation } from 'react-query';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { api } from '~/services/api';
+import { queryClient } from '~/services/queryClient';
+import { useRouter } from 'next/router';
 
 type CreateUserFormData = {
   name: string;
@@ -26,12 +30,45 @@ const signUpFormSchema = yup.object().shape({
 });
 
 export default function CreateUser() {
+  const toast = useToast();
+  const router = useRouter();
+  
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date(),
+      }
+    });
+
+    return response.data.user;
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users');
+    }
+  });
+  
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(signUpFormSchema)
   });
   
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = (values) => {
-    console.log(values);
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    try {
+      await createUser.mutateAsync(values);
+      toast({
+        status: 'success',
+        title: 'Sucesso',
+        description: 'O usuário foi cadastrado com sucesso!'
+      });
+
+      router.push('/users');
+    } catch (err) {
+      toast({
+        status: 'error',
+        title: 'Erro',
+        description: 'Ocorreu um erro durante a criação do usuário!'
+      });
+    }
   }
 
   return (
